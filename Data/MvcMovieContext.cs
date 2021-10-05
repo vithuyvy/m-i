e3692febcde613321baeb7c32cde5496192555af
ViVy <vyvince1907@gmail.com>
2021-10-05 23:04:58 +0700

    first commit

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using MvcMovie.Data;
+
+    namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
